2cfecbc724f519cee0d42ecd796e53cbf30eb5b1
dozono <kamidozono@daas.co.jp>
2024-01-06 02:15:40 +0900

    wip

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,25 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         authentication_keys: [:login]
+
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+  validates :username, format: { with: /^[a-zA-Z0-9_\.]*$/, multiline: true }
+
+  attr_writer :login
+
+  def login
+    @login || username || email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if (login = conditions.delete(:login))
+      where(conditions.to_h).where(['lower(username) = :value OR lower(email) = :value',
+                                    { value: login.downcase }]).first
+    elsif conditions.key?(:username) || conditions.key?(:email)
+      where(conditions.to_h).first
+    end
+  end
 end
