47cb50a9436b4b18b330a130215f89328c83675a
Dozono <31640715+dozonot@users.noreply.github.com>
2024-01-07 06:05:22 +0900

    fix: device deprecation warning (#28)

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,6 +9,9 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+  # NOTE: https://github.com/heartcombo/devise/issues/5644
+  config.secret_key = Rails.application.secret_key_base
+
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
