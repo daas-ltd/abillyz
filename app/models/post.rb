51974e196cbd10f8594270d067d089b74a5a3b2c
dozono <kamidozono@daas.co.jp>
2024-01-08 09:55:51 +0900

    feat: post card

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,19 @@
 # frozen_string_literal: true
 
 class Post < ApplicationRecord
+  before_create :set_published_at
   belongs_to :user
 
   has_one_attached :thumbnail
 
   validates :title, presence: true, length: { maximum: 255 }
   validates :body, presence: true
+
+  scope :published, -> { where(published: true) }
+
+  private
+
+  def set_published_at
+    self.published_at = DateTime.current if published_at.nil? && published == true
+  end
 end
