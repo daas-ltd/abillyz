c3642234a8d4a1f705419c6209024906663eb6d7
Dozono <kamidozono@daas.co.jp>
2024-09-24 04:02:07 +0900

    feat: Upgrade Rails 7.2.1

--- config/application.rb
+++ config/application.rb
@@ -21,12 +21,13 @@ Bundler.require(*Rails.groups)
 module Abillyz
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    config.load_defaults 7.2
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_lib(ignore: %w[assets tasks])
+
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -36,8 +37,6 @@ module Abillyz
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    
-    
     # add rails generators config, remove test files
     config.generators do |g|
       g.test_framework  :rspec,
